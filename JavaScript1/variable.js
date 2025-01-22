6f6d9236db8f1991194780f9c8e55df6d32d4bad
Inertia3 <sahilansari37863@gmail.com>
2025-01-22 23:52:06 +0530

    Variables in JS

---
+++ JavaScript1/variable.js
@@ -0,0 +1,7 @@
+var a = 50;
+let b = 60;
+const c = 70;
+// Variable in java can be created by these three ways
+console.log(a, b, c);
+// Var type can be either function scoped or global scoped and not blocked scope also re assignment is possible
+// let type is only blocked scope
